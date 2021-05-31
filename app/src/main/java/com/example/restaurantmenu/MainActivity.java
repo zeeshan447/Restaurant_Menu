c45f517eaac59e5120e6f918e12d1d4d2f5c6a7e
zeeshan447 <zeeshananwar2011@gmail.com>
2021-05-31 14:31:11 +0500

    Sign in and signup page

--- app/src/main/java/com/example/restaurantmenu/MainActivity.java
+++ app/src/main/java/com/example/restaurantmenu/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.restaurantmenu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button btnSignup, btnSignIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnSignup = findViewById(R.id.signUp);
+        btnSignIn = findViewById(R.id.signIn);
+        btnSignup.setOnClickListener(this);
+        btnSignIn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch ((view.getId())){
+            case R.id.signUp:
+                Intent intent = new Intent(this, Signup.class);
+                startActivity(intent);
+                break;
+            case R.id.signIn:
+                Intent intent1 = new Intent (this, SignIn.class);
+                startActivity(intent1);
+                break;
+        }
     }
 }
